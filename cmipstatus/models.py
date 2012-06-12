35411fb318772e0ce8772b351e7cfb22a27fccc9
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-06-12 16:12:45 -0300

    barras de progresso

--- cmipstatus/models.py
+++ cmipstatus/models.py
@@ -1,28 +1,38 @@
 from django.db import models
-from fabric.api import run, env, settings
+from fabric.api import run, env, settings, get
 from datetime import datetime
+
+RESTART_LIST_TEMPLATE = '/stornext/online2/ocean/simulations/{0}/experiment_design/RESTARTLIST.{1}.tmp'
 
 class Experiment(models.Model):
     name = models.CharField(max_length=15)
 
-    def check_status(self, tupa_data):
-        #check for itself
-        if tupa_data:
-            lines = tupa_data.split('\n')
-            lines.pop(0)
-            for line in lines:
-                columns = line.split()
-                if len(columns) > 0 and columns[1].endswith(self.name):
-                    return [columns]
-        #if not itself, check for member
-        members = Member.objects.all().filter(exp=self)
-        if not members: #no members, no go
-            return None
-        members_data = []
-        for member in members:
-            members_data.append(member.check_status(tupa_data))
-        return members_data
+    def get_status(self, tupa_data):
+        done, total = check_restart_list(self.name, '')
+        current = check_status(self.name, '', tupa_data)
+        print "done, total", done, total
+        return done, total, current
 
+    '''
+    def check_status(self, tupa_data):
+        members = Member.objects.all().filter(exp=self)
+        if not members: #no members, check itself
+            #check for itself
+            if tupa_data:
+                lines = tupa_data.split('\n')
+                lines.pop(0)
+                for line in lines:
+                    columns = line.split()
+                    if len(columns) > 0 and columns[1].endswith(self.name):
+                        return [columns]
+        else:
+            members_data = []
+            for member in members:
+                members_data.append(member.check_status(tupa_data))
+                member.check_restart_list()
+            return members_data
+        return None #last case
+    '''
     def __unicode__(self):
         return self.name
 
@@ -31,21 +41,43 @@ class Member(models.Model):
     name = models.CharField(max_length=2)
     exp = models.ForeignKey('Experiment')
 
-    def check_status(self, tupa_data):
-        print "searching", self.exp.name + self.name
-        if tupa_data:
-            lines = tupa_data.split('\n')
-            lines.pop(0)
-            for line in lines:
-                columns = line.split()
-                if len(columns) > 0 and columns[1].endswith(self.exp.name + self.name):
-                    print "found", columns
-                    return columns
-        print "not found"
-        return [None, self.exp.name+self.name, None, None, '0%']
+    def get_status(self,tupa_data):
+        done, total = check_restart_list(self.exp.name, self.name)
+        current = check_status(self.exp.name, self.name, tupa_data)
+        print "done, total", done, total
+        return done, total, current
 
     def __unicode__(self):
         return self.name
+
+
+def check_restart_list(exp_name, member_name):
+    file_to_read = RESTART_LIST_TEMPLATE.format(exp_name, exp_name+member_name)
+    with settings(host_string='ocean@tupa', warn_only=True):
+        get(file_to_read, '.')
+        print "getting..."
+        restart_list = open("RESTARTLIST.{0}.tmp".format(exp_name+member_name), 'r')
+        restarts = 0.
+        done = 0.
+        for line in restart_list:
+            restarts += 1
+            if 'END' in line:
+                done += 1
+        return done, restarts
+
+
+def check_status(exp_name, member_name, tupa_data):
+    print "searching", exp_name+member_name
+    if tupa_data:
+        lines = tupa_data.split('\n')
+        lines.pop(0)
+        for line in lines:
+            columns = line.split()
+            if len(columns) > 0 and columns[1].endswith(exp_name+member_name):
+                print "found", columns
+                return columns
+    print "not found"
+    return [None, exp_name+member_name, None, None, '0%']
 
 
 class TupaQuery(models.Model):
@@ -69,3 +101,5 @@ class TupaQuery(models.Model):
                 print "Failed to refresh data"
             self.save()
         return self.query_result
+
+
--- cmipstatus/views.py
+++ cmipstatus/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from models import Experiment, TupaQuery
+from models import Experiment, TupaQuery, Member
 
 def home(request):
     return render_to_response("cmiphome.html", {})
@@ -12,30 +12,39 @@ def explist(request):
 
 def expview(request, expname):
     exp = Experiment.objects.get(name=expname)
+    members = Member.objects.all().filter(exp=exp)
+    exp = [exp]
     tupa_data = TupaQuery.objects.get(name='query').get_data()
-    members_info = exp.check_status(tupa_data)
     runinfo = []
     info = {'exp':exp}
 
-    if members_info:
-        for member_info in members_info:
-            print "showing", member_info
-            minfo = {'member':member_info[1].split('_')[-1]}
-            if member_info[0] == None: #no job, no job_id
-                 minfo['submitted'] = False
-            else:
-                minfo['submitted'] = True
-                minfo['job_id'] = member_info[0]
-                if 'aux' in minfo['job_id']:
-                    minfo['post'] = True
-                    minfo['status'] = 1.0
-                else:   
-                    minfo['post'] = False
-                    print "status", member_info[-1][:-1]
-                    minfo['status'] = float(member_info[-1][:-1])/100
-            runinfo.append(minfo)
-        info['minfo'] = runinfo
-    else:
-        info['minfo'] = None
+    if members:
+        exp = members
 
+    for member in exp:
+        done, total, current = member.get_status(tupa_data)
+        finished_prog = done / total
+        run_fraction = 1. / total
+        minfo = {'member':current[1].split('_')[-1], 'current':done+1, 'total':total}
+        if current[0] == None: #not running
+            minfo['running'] = False
+            minfo['prog'] = 0.0 * run_fraction + finished_prog
+            minfo['text_run'] = '0'
+            minfo['text_total'] = "%3.2f" % (minfo['prog']*100)
+        else:
+            minfo['running'] = True
+            minfo['job_id'] = current[0]
+            if 'aux' in current[0]:
+                minfo['post'] = True
+                minfo['text_run'] = '100'
+                minfo['prog'] = 1.0 * run_fraction + finished_prog
+                minfo['text_total'] = "%3.2f" % (minfo['prog']*100)
+            else:
+                minfo['post'] = False
+                minfo['text_run'] = current[-1][:-1]
+                minfo['prog'] = float(minfo['text_run'])/100.0 * run_fraction + finished_prog
+                minfo['text_total'] = "%3.2f" % (minfo['prog']*100)
+        runinfo.append(minfo)
+
+    info['minfo'] = runinfo
     return  render_to_response("cmipexpview.html", info)
