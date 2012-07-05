a7b3de54f3013b9baeac53ba6c4f4aeda32caa6b
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-07-05 09:58:11 -0300

    fixing new figures move

--- cmipstatus/cmip_bots/cmip_server_bot.py
+++ cmipstatus/cmip_bots/cmip_server_bot.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
             subprocess.call(['rm', '-rf', old_dir])
         for cmpdir in glob.glob('fetched_data/cmip_evaluation/cmp*'):
             subprocess.call(['mv', cmpdir, '../../media'])
+        subprocess.call(['rm', '-rf', '../../media/exp_analysis_figures'])
         subprocess.call(['mv', 'fetched_data/cmip_evaluation/exp_analysis_figures','../../media'])
         time.sleep(150)
