e3e9903fac78afeb8a895a03b5d1c1b395e85fc8
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-07-19 14:27:32 -0300

    adding django-cronjobs to installed apps

--- settings.py
+++ settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cmipstatus',
+    'cronjobs',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
