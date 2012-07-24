e542fd8d89c9e2a9ddca2b6a9507be481e2f795f
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-07-24 17:21:59 -0300

    unicode representation for all models (better on admin)

--- groupsite/models.py
+++ groupsite/models.py
@@ -15,12 +15,21 @@ class NewsImg(models.Model):
     news = models.ForeignKey('News')
     img = models.ImageField(max_length=1024, upload_to='media')
 
+    def __unicode__(self):
+        return self.news.title
+
 
 class ScienceThing(models.Model):
     short = models.CharField(max_length=256)
     description = models.CharField(max_length=2048)
 
+    def __unicode__(self):
+        return self.short
+
 
 class YoutubeVideo(models.Model):
     science_thing = models.ForeignKey('ScienceThing')
     video_link = models.CharField(max_length=1024)
+
+    def __unicode__(self):
+        return self.science_thing.short
