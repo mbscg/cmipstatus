afd34aa72c75faa9aa097a8e92555d0e2e3ecaef
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-07-25 17:20:14 -0300

    thumbnailind the photos, faster loading!

--- settings.py
+++ settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'groupsite',
     'cronjobs',
     'south',
+    'sorl.thumbnail',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
