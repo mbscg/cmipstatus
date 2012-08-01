3adea94652c3a2c40bafe9d72950e2548105db66
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-08-01 14:00:05 -0300

    added verbose name for People model in cmipstatus app

--- cmipstatus/models.py
+++ cmipstatus/models.py
@@ -25,9 +25,10 @@ CONVERT_CHOICES = (
 
 
 class People(User):
-    name = models.CharField(max_length=100)
-    about = models.TextField(default='INPE GMAO Researcher')
-    photo = models.ImageField(max_length=1048, upload_to='media', default='profile_default.png')
+    name = models.CharField(max_length=100, verbose_name="Nome")
+    about = models.TextField(default='INPE GMAO Researcher', verbose_name="Sobre")
+    photo = models.ImageField(max_length=1048, upload_to='media', 
+        default='profile_default.png', verbose_name="Foto")
 
     def __unicode__(self):
         return self.name
