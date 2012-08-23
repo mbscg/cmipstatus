94edac760d63dba04b4d1f17f79b5ffaf3901eda
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2012-08-23 10:05:01 -0300

    changing gathered info on conversion progress

--- cmipstatus/cmip_bots/cmip_restart_bot.py
+++ cmipstatus/cmip_bots/cmip_restart_bot.py
@@ -24,49 +24,36 @@ def get_conversion_status():
                  'decadal1990', 'decadal1995', 'decadal2000']
     decades = decades30 + decades10
 
-    #root = '/stornext/online13/ocean/simulations/cmip5/CMIP5/output/INPE/INPE-OA2-3/'
     root = '/stornext/online13/ocean/simulations/CMIP/CMIP5/output/INPE/BESM-OA2-3/'
     frequency = 'mon'
-    condition = 'r{0}i1p1'
+    condition = 'r{0}i1p{1}'
+    fmt_condition = 'r{0} i1 p{1}'
     variables = [('atmos', 'tas', 40), ('ocean', 'tos', 42), ('land', 'mrsos', 2)]
 
     text_lines = []
 
-    total_weights = 40 + 42 + 2
     for decade in decades:
-        decade_total = 0
-        variables_read = 0
-        for variable in variables:
-            var_total = 0
-            conditions_read = 0
-            for cond in range(1,11):
-                c = condition.format(str(cond))
-                directory = os.path.join(root, decade, frequency, variable[0], variable[1], c)
-                try:
-                    ls = os.listdir(directory)
-                    var_total += len(ls)
-                    conditions_read += 1
-                except:
-                    pass
-                print "cond, var, decade, len(ls)", cond, variable, decade, len(ls)
-            if conditions_read > 0:
-                var_average = float(var_total) / conditions_read
-            else:
-                var_average = 0
-            decade_total += var_average * variable[2]/total_weights
-            variables_read += 1
-        if variables_read == 0:
-            text_lines.append(' '.join([decade, 'NOT FOUND', '\n']))
-            continue
+        decade_expected = 120.
+        if decade_expected in decades30:
+            decade_expected = 360.
+        for r in range(1,11): #r1 a r10
+            for p in range(1,3): #p1 e p2
+                cond = condition.format(r, p)
+                fmt_cond = fmt_condition.format(r, p)
+                cond_total = 0.
+                cond_weights = 0.
+                for var in variables:
+                    directory = os.path.join(root, decade, frequency, var[0], var[1], cond)
+                    try:
+                        ls = os.listdir(directory)
+                    except:
+                        continue
+                    else:
+                        var_total = len(ls)
+                        progress = str(var_total / decade_expected)
+                        line = ' '.join([decade, fmt_cond, str(var_total), str(decade_expected), progress, '\n'])
+                        text_lines.append(line)
 
-        expected = 120.0
-        if decade in decades30:
-            expected = 360.0
-        current = float(decade_total)
-        print "decade total, expected", decade_total, expected
-        progress = current/expected
-        text_lines.append(' '.join([decade, str(current), str(expected), 
-                          str(progress), '\n']))
 
     DEST_PATH = os.path.join(all_info['paths']['ftp_root'], 'conversion.txt')
     DEST_FILE = open(DEST_PATH, 'w')
