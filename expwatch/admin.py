25be9aaf8de5c99842aa1161c7fd95e3fb78e283
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-08-02 11:01:00 -0300

    adding Alert to adminwq

--- expwatch/admin.py
+++ expwatch/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from expwatch.models import Exp, ExpMember
+from expwatch.models import Exp, ExpMember, Alert
 
 admin.site.register(Exp)
 admin.site.register(ExpMember)
+admin.site.register(Alert)
