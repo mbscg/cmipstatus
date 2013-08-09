4df7ded1edb7610791ecf23e10fc3e1a3dce6924
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-08-09 13:56:23 -0300

    docstrings on officeboy

--- expwatch/officeboy.py
+++ expwatch/officeboy.py
@@ -1,14 +1,23 @@
 from settings import officeboy_configs as config
 
 def get_tupa_data():
+    """
+    retrieves the running stats
+    """
     log_file = open(config['qstat_log'], 'r')
     log_text = log_file.readlines()
     log_file.close()
     return log_text
 
+
 def get_restart_list(fancy_name):
+    """
+    retrieves the restart list for a given exp/member
+    """
     fancy_filename = config['restart_list_template'].format(fancy_name)
     restart_file = open(fancy_filename, 'r')
     restart_list = restart_file.readlines()
     restart_file.close()
     return restart_list
+
+
