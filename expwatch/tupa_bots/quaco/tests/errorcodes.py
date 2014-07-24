f86289065c66ee2c2fa25222951ebc4866f807bf
Ocean <ocean@cptec.inpe.br>
2014-07-24 10:17:58 -0300

    new errorcodes

--- expwatch/tupa_bots/quaco/tests/errorcodes.py
+++ expwatch/tupa_bots/quaco/tests/errorcodes.py
@@ -2,6 +2,7 @@ FILESIZE = 1
 MINVALUE = 2
 LOADERR = 4
 ALLZERO = 8
+STD0ERR = 16
 
 
 descriptions = {
@@ -9,10 +10,11 @@ descriptions = {
     MINVALUE: 'min value too high (possibly all undefined)',
     LOADERR: 'could not open file',
     ALLZERO: 'all values zero',
+    STD0ERR: 'standard deviation is zero',
 }
 
 
 def decode(code):
-    error_list = [code & error for error in [1,2,4,8]]
+    error_list = [code & error for error in descriptions.keys()]
     desc_list = [descriptions[error] for error in error_list if error]
     return desc_list
